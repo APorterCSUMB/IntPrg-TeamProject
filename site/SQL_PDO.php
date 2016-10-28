13d106c1a0d70c303e0323785461de274f1d57b5
Andrew <aporter@csumb.edu>
2016-10-28 20:34:55 +0000

    Andrew ready

--- site/SQL_PDO.php
+++ site/SQL_PDO.php
@@ -1,16 +1,21 @@
 <?php
-    $host = getenv("IP");
+
+    // NO TEAM MEMBER SHALL EDIT THIS FILE WITHOUT CONFERENCING THE REST OF THE TEAM FIRST
+    // YOU WILL BE FIRED AND WE WILL TAKE BACK ALL OF YOUR BONUS PAY FROM THE LAST SIX MONTHS
+    
+    $host = 'https://intprg-wspace-aporter87.c9users.io/';
     $port = 3306;
-    $db = "Lab5";
-    $user = getenv('C9_USER');
+    $db = "TeamProject";
+    $user = "aporter87";
     $pass = "";
     $sql = '**NO DB HANDLE**';
     $flag = false;
     try {
-        $sql = new PDO('mysql:host='.$host.';dbname='.$db.';port='.$port.';', $user, $pass);
+        $sql = new PDO('mysql: host='.$host.';dbname='.$db.';port='.$port.';', $user, $pass);
         $sql -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $flag = true;
     } catch (PDOException $x) {
         echo '<div style="color:green;font-weight:bold;">--> Unable to connect to SQL DB!</div>';
     }
+
 ?>
