8d29c80f5f996de78a85ffff960403a00bd1e28a
Andrew <aporter@csumb.edu>
2016-10-28 02:46:06 +0000

    SQL PDO Fixed, Single DB Reference

--- site/SQL_PDO.php
+++ site/SQL_PDO.php
@@ -1,16 +1,22 @@
 <?php
-    $host = getenv("IP");
-    $port = 3306;
-    $db = "TeamProject";
-    $user = getenv('C9_USER');
+    $dsn = 'mysql: host=https://intprg-wspace-aporter87.c9users.io/;port=3306;dbname=TeamProject';
+    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+    $user = "aporter87";
     $pass = "";
     $sql = '**NO DB HANDLE**';
     $flag = false;
     try {
-        $sql = new PDO('mysql:host='.$host.';dbname='.$db.';port='.$port.';', $user, $pass);
+        $sql = new PDO($dsn, $user, $pass, $options);
         $sql -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $flag = true;
     } catch (PDOException $x) {
         echo '<div style="color:green;font-weight:bold;">--> Unable to connect to SQL DB!</div>';
     }
+    
+    
+
+
+
+
+
 ?>
