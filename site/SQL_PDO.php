5af3ce07aa690d16928977a9234fd468aef37b65
Andrew <aporter@csumb.edu>
2016-10-28 16:31:38 +0000

    PDO Style Reversion, Workspace Ready for Project

--- site/SQL_PDO.php
+++ site/SQL_PDO.php
@@ -1,22 +1,21 @@
 <?php
-    $dsn = 'mysql: host=https://intprg-wspace-aporter87.c9users.io/;port=3306;dbname=TeamProject';
-    $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
+
+    // NO TEAM MEMBER SHALL EDIT THIS FILE WITHOUT CONFERENCING THE REST OF THE TEAM FIRST
+    // YOU WILL BE FIRED AND WE WILL TAKE BACK ALL OF YOUR BONUS PAY FROM THE LAST SIX MONTHS
+    
+    $host = 'https://intprg-wspace-aporter87.c9users.io/';
+    $port = 3306;
+    $db = "TeamProject";
     $user = "aporter87";
     $pass = "";
     $sql = '**NO DB HANDLE**';
     $flag = false;
     try {
-        $sql = new PDO($dsn, $user, $pass, $options);
+        $sql = new PDO('mysql: host='.$host.';dbname='.$db.';port='.$port.';', $user, $pass);
         $sql -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         $flag = true;
     } catch (PDOException $x) {
         echo '<div style="color:green;font-weight:bold;">--> Unable to connect to SQL DB!</div>';
     }
-    
-    
-
-
-
-
 
 ?>
