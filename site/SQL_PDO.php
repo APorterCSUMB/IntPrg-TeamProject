312dfd77527a88a4d0d70efbdb26354057d7e6e3
Brandon <bavery@csumb.edu>
2016-10-28 20:41:09 +0000

    cleanup

---
+++ site/SQL_PDO.php
@@ -0,0 +1,16 @@
+<?php
+    $host = getenv("IP");
+    $port = 3306;
+    $db = "TeamProject";
+    $user = getenv('C9_USER');
+    $pass = "";
+    $sql = '**NO DB HANDLE**';
+    $flag = false;
+    try {
+        $sql = new PDO('mysql:host='.$host.';dbname='.$db.';port='.$port.';', $user, $pass);
+        $sql -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $flag = true;
+    } catch (PDOException $x) {
+        echo '<div style="color:green;font-weight:bold;">--> Unable to connect to SQL DB!</div>';
+    }
+?>
