065b0047d58264dd686bb5f127610c5ae2936651
Andrew <aporter@csumb.edu>
2016-11-09 09:51:12 +0000

    Final Master

--- site/index.php
+++ site/index.php
@@ -15,6 +15,8 @@
                 $sortTmp = $_POST["sort"];
                 $priceTmp = $_POST["price"];
                 $healthyTmp = $_POST["healthy"];
+                $lowCal = $_POST["lowCal"];
+                $fullMeal = $_POST["fullMeal"];
                 
                 // ERROR BLOCK, ALL ERRORS SET CUSTOM ERROR MESSAGES
                 // - SORT PARSING -
@@ -33,6 +35,15 @@
                 }
                 else{$healthy = "";}
                 
+                if(!strcmp($lowCal, 'set')){
+                    $lowCal = 'set';
+                }
+                else{$lowCal = "";}
+                
+                if(!strcmp($fullMeal, 'set')){
+                    $fullMeal = 'set';
+                }
+                else{$fullMeal = "";}
                 
             }
             
@@ -50,12 +61,21 @@
                             <input type="radio" name="sort" value="price"'.(!strcmp($sort,"price")?'checked':'').'>Price
                             <br>
                             <input type="radio" name="sort" value="TypeDesc"'.(((strcmp($sort,'productName'))&&(strcmp($sort,'price')))?'checked':'').'>Type
+                            <br><br>
+                            ORDER BY: <select name = "orderby" size=1>
+                            <option value= "ASC"> ASC</option>
+                            <option value = "DESC"> DESC</option>
+                            </select>
                         </td>
                         <td>
-                            FILTER BY:<br><br>
-                            Max Price: $<input type="text" name="price" style="width:50px" value='.($price?$price:'').'>
-                            <br><br>
-                            Healthy Choice: <input type="checkbox" name="healthy" value="set" '.(!strcmp($healthyTmp, 'set')?'checked':'').'>
+                            <fieldset>
+                                <legend>Filter By:</legend>
+                                Max Price: $<input type="text" name="price" style="width:50px" value='.($price?$price:'').'>
+                                <br><br>
+                                Healthy Choice: <input type="checkbox" name="healthy" value="set" '.(!strcmp($healthyTmp, 'set')?'checked':'').'><br>
+                                Low Calorie Choice: <input type="checkbox" name="lowCal" value="set" '.(!strcmp($lowCal, 'set')?'checked':'').'><br>
+                                Full Meal Choice: <input type="checkbox" name="fullMeal" value="set" '.(!strcmp($fullMeal, 'set')?'checked':'').'>
+                            </fieldset>
                         </td>
                         <td>
                             <input type="submit" value="SORT THE MENU"></form>
@@ -66,10 +86,10 @@
                         </td>
                     </tr>
                 </table>';
-                $stmt = "select * from Products natural join ProductType order by price";
+                $stmt = "select * from Products natural join ProductType natural join Producer order by price";
                 $id = $name = $description = $cost = $calories = $health = $productID = "";
                 if($_SERVER["REQUEST_METHOD"] == "POST"){
-                    $stmt = "select * from Products natural join ProductType";
+                    $stmt = "select * from Products natural join ProductType natural join Producer";
                     $flag = false;
                     // TODO - FILTER BASED ON SIMPLE CHECKBOX
                     if(!empty($price)){
@@ -82,12 +102,34 @@
                         }
                         else{
                             $stmt = $stmt." WHERE healthyChoice=1";
+                            $flag = true;
                         }
                     }
-                    if(!empty($sort)){
-                        $stmt = $stmt." ORDER BY ".$sort;
+                    if(!empty($lowCal)){
+                        if($flag){
+                            $stmt = $stmt." AND calories <= 200";
+                        }
+                        else{
+                            $stmt = $stmt." WHERE calories <= 200";
+                            $flag = true;
+                        }
                     }
-                    // TODO - SORT ASC OR DESC BASED ON RADIO BUTTON
+                    if(!empty($fullMeal)){
+                        if($flag){
+                            $stmt = $stmt." AND TypeID=10300";
+                        }
+                        else{
+                            $stmt = $stmt." WHERE TypeID=10300";
+                        }
+                    }
+                    if(!empty($sort)&& $_POST["orderby"] == "ASC"){
+                        $stmt = $stmt." ORDER BY ".$sort;
+                         $stmt = $stmt. " ASC";
+                    }
+                    else{
+                         $stmt = $stmt." ORDER BY ".$sort;
+                        $stmt = $stmt. " DESC";
+                    }
                 }
                 $stmt = $sql->prepare($stmt);
                 $flag = $stmt->execute();
@@ -112,6 +154,9 @@
                                 Type
                             </td>
                             <td>
+                                Producer
+                            </td>
+                            <td>
                                 Buy It
                             </td>
                         </tr>
@@ -127,6 +172,7 @@
                             echo '<td class="info"><img src="happy.png"></td>';
                         } else{echo '<td class="info"><img src="sad.png"></td>';}
                         echo '<td class="info">'.$product['TypeDesc'].'</td>';
+                        echo '<td class="info">'.$product['producerName'].'</td>';
                         echo '<td class="info"><form method="POST" action="cart.php"><input type="submit" name="'.$product['productID'].'" value="Add to Cart"></form></td>';
                     echo '</tr>';
                 }
